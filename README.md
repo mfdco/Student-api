bda6e1d8b9604fe6b3b6181d210a17b589455874
Marcos Felipe de Castro Ornelas <mornelas@mocs.flsouthern.edu>
2026-02-23 19:09:19 -0500

    Routes.py finished, along with README.md file

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# Student-api
+REST API HOMEWORK ASSIGNMENT
+
+1.) Project Description
+   ---------------------
+This project contains a REST API used to manage and create student records using FAST API and SQLite.It allows the user to create, retrieve, delete, or update student information in the database. The purpose of this project is to better understand how APIs work along with how they can be used with a databse. 
+
+=========================================================================
+
+2.) Installation Instructions
+    --------------------------
+Step 1 : Clone the repository and cd into the file
+	a.) git clone <https://github.com/mfdco/Student-api.git>
+	b.) cd student-api
+
+Step 2 : Create a virtual environment
+	a.) python -m venv .venv
+	b.1) source .venv/bin/activate (Mac/Linux)
+	b.2) .venv\Scripts\activate (Windows)
+
+Step 3 : Install requirements
+	a.) pip install -r requirements.txt
+
+Step 4 : Start up FASTAPI
+	a.) uvicorn mail:app --reload
+
+Step 5 : Click on the webstite's url
+	a.) Copy and paste http://127.0.0.1.8000 into a url
+	b.) add /docs to the end of the url to enter into the API documentation and test the API
+
+=========================================================================
+
+3.) API endpoints
+   --------------- 
+ Method | Endpoint        | Description                 | Codes
+________|_________________|_____________________________|______ 
+  GET   |/student         |gets all students            |200
+  GET   |/student/{id}    |get specific student by id   |200, 404
+  GET   |/student/by-major|get specific student by major|200
+  GET   |/student/by-gpa  |get specific student by gpa  |200, 404
+  POST  |/studen          |create student               |201, 400
+  PUT   |/student/{id}    |update student               |200, 400, 404
+ DELETE |/student/{id}    |delete student               |200/204, 404
+
+=========================================================================
+
+4.) Testing instructions
+   ----------------------
+Step 1 : Start server
+	a.) refer to Running Server
+
+Step 2 : Test the api within documentation
+	a.) Start with create student 
+	b.) Use the other endpoints the modify, get, or delete the student record(s)
+
+========================================================================= 
+
+5.) Examples
+
+Example 1 (Create Student) :
+  
+	curl -X 'POST' \ 
+	  'http://127.0.0.1:8000/students' \ 
+	  -H 'accept: application/json' \ 
+	  -H 'Content-Type: application/json' \ 
+	  -d '{ 
+	  "id": 0, 
+	  "name": "Lee Roy", 
+	  "email": "leyroy8@gmail.com", 
+	  "major": "Business Administration", 
+	  "gpa": 3.1, 
+	  "enrollment_year": 1 
+        }'
+
+Example 2 (Get all students) :
+ 
+	curl -X 'GET' \
+  	  'http://127.0.0.1:8000/students' \
+  	  -H 'accept: application/json'
+
+Example 3 (Get one student by id) : 
+	
+	curl -X 'GET' \
+  	  'http://127.0.0.1:8000/student/1' \
+  	  -H 'accept: application/json'
+
+=========================================================================
--- routes.py
+++ routes.py
@@ -4,37 +4,197 @@ from database import get_connection
 
 router = APIRouter()
 
-@router.get("???") # TODO: Replace ??? with correct endpoint path
+@router.get("/students") # TODO: Replace ??? with correct endpoint path
 def get_all_students():
-    # TODO: Implement this
-    pass
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM students")
+
+    row = cursor.fetchall()
+
+    students = []
+
+    count = 0
     
-@router.get("???") # TODO: Replace ??? with correct endpoint path
+    for i in range(len(row)):
+        students.append(row[i])
+        count += 1
+        
+    conn.commit()
+    conn.close()
+
+    return {
+        "students": students,
+        "count": count,
+    }
+    
+@router.get("/students/by-major") # TODO: Replace ??? with correct endpoint path
 def get_students_by_major(major: str):
-    # TODO: Implement this
-    pass
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM students WHERE major = '{major}'")
+
+    row = cursor.fetchall()
+
+    names = []
+
+    count = 0
+
+    for i in range(len(row)):
+        if row[i][3] == major:
+            names.append(row[i][1])
+            count += 1
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "name": names,
+        "count": count,
+        "major": major,
+    }
     
-@router.get("???") # TODO: Replace ??? with correct endpoint path
+@router.get("/students/by-gpa") # TODO: Replace ??? with correct endpoint path
 def get_students_by_gpa(min_gpa: float):
-    # TODO: Implement this
-    pass
     
-@router.get("???") # TODO: Replace ??? with correct endpoint path
+    if min_gpa < 0.0 or min_gpa > 4.0:
+        raise HTTPException(status_code = 400, detail = f"GPA must be between 0.0 and 4.0")
+    
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT * FROM students WHERE gpa >= {min_gpa}")
+
+    row = cursor.fetchall()
+
+    count = 0
+
+    names = []
+    
+    for i in range(len(row)):
+        if row[i][4] >= min_gpa:
+            names.append(row[i][1])
+            count += 1
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "name": names,
+        "count": count,
+        "major": min_gpa,
+    }
+    
+@router.get("/student/{student_id}") # TODO: Replace ??? with correct endpoint path
 def get_student(student_id: int):
-    # TODO: Implement this
-    pass
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if not cursor.execute(f"SELECT * FROM students WHERE id = {student_id}").fetchone():
+        raise HTTPException(status_code = 404, detail = f"Student with ID {student_id} not found")
+
+    cursor.execute(f"SELECT * FROM students WHERE id = {student_id}")
+
+    row = cursor.fetchone()
+
+    student_name = row[1]
+    student_email = row[2]
+    student_major = row[3]
+    student_gpa = row[4]
+    student_enrollment = row[5]
     
-@router.post("???", status_code=201) # TODO: Replace ??? with correct endpoint path
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "id": student_id,
+        "name": student_name,
+        "email": student_email,
+        "major": student_major,
+        "gpa": student_gpa,
+        "enrollment_year": student_enrollment
+    } 
+
+    
+@router.post("/students", status_code=201) # TODO: Replace ??? with correct endpoint path
 def create_student(student: Student):
-    # TODO: Implement this
-    pass
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if student.gpa < 0.0 or student.gpa > 4.0:
+        raise HTTPException(status_code = 400, detail = "GPA must be between 0.0 and 4.0")
+
+    elif not student.name or not student.name.strip():
+        raise HTTPException(status_code = 400, detail = "Name field is empty, put in a name")
+
+    elif not student.major or not student.major.strip():
+        raise HTTPException(status_code = 400, detail = "Major field is empty, put in a major")
+
+    cursor.execute("INSERT INTO students (name, email, major, gpa, enrollment_year)" f"VALUES ('{student.name.strip()}', '{student.email}', '{student.major.strip()}', {student.gpa}, {student.enrollment_year})")
+
+    row = cursor.fetchone()
     
-@router.put("???") # TODO: Replace ??? with correct endpoint path
+    conn.commit()
+    conn.close()
+    
+    return {
+        "id": student.id,
+        "name": student.name,
+        "email": student.email,
+        "major": student.major,
+        "gpa": student.gpa,
+        "enrollment_year": student.enrollment_year
+    }
+    
+    
+@router.put("/students/{student_id}") # TODO: Replace ??? with correct endpoint path
 def update_student(student_id: int, student: Student):
-    # TODO: Implement this
-    pass
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if not cursor.execute(f"SELECT * FROM students WHERE id = {student_id}").fetchone():
+        raise HTTPException(status_code = 404, detail = f"Student with ID {student_id} not found")
+
+    elif student.gpa < 0.0 or student.gpa > 4.0:
+        raise HTTPException(status_code = 400, detail = "GPA must be between 0.0 and 4.0")
+
+    elif not student.name or not student.name.strip():
+        raise HTTPException(status_code = 400, detail = "Name field is empty, put in a name")
+
+    elif not student.major or not student.major.strip():
+        raise HTTPException(status_code = 400, detail = "Major field is empty, put in a major")
+
+    cursor.execute(
+           f"UPDATE students SET name = '{student.name.strip()}', email = '{student.email}', major = '{student.major.strip()}', gpa = {student.gpa}, enrollment_year = {student.enrollment_year} WHERE id = {student_id}")
+
+    row = cursor.fetchone()
     
-@router.delete("???") # TODO: Replace ??? with correct endpoint path
+    conn.commit()
+    conn.close()
+    
+    return {
+        "id": student.id,
+        "name": student.name,
+        "email": student.email,
+        "major": student.major,
+        "gpa": student.gpa,
+        "enrollment_year": student.enrollment_year
+    } 
+    
+@router.delete("/students/{student_id}")
 def delete_student(student_id: int):
-    # TODO: Implement this
-    pass
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    if not cursor.execute(f"SELECT * FROM students WHERE id = {student_id}").fetchone():
+        raise HTTPException(status_code = 404, detail = f"Student with ID {student_id} not found")
+
+    cursor.execute(f"DELETE FROM students WHERE id = {student_id}")
+
+    conn.commit()
+    conn.close()
+
+    return {"message": "Student deleted successfully"}
